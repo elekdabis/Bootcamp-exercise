beb7ae564eb57deb2d02a0cceb3502c2728601e4
elekdabis <elekdabis@yahoo.com>
2019-04-05 20:37:26 +0200

    ispravka

--- ITBootcamp-Project/src/utility/Constant.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-public class Constant {
-	public static final String PAGE_URL="http://localhost/izlet/index.php";
-	public static final String path="D:\\IT Bootcamp\\";
-	public static final String data="Data.xls";
-	public static final String sheetNameRegistracija="Registracija";
-	public static final String sheetNameTekst="Tekst";
-}
